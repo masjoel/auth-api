aef7c96154ace639db9d3c47125c132d3c15e8c3
masjoel <masjoel@gmail.com>
2023-04-27 22:16:49 +0700

    pass say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -531,6 +531,7 @@ describe('HTTP server', () => {
         method: 'GET',
         url: '/',
       });
+      
       // Assert
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(200);
